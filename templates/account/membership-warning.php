f294309cdd8821ed8d864b8182bb17f5f9904868
Hoang Huu <hoanghn@brainos.vn>
2019-09-30 16:43:46 +0700

    Update logic free package

--- templates/account/membership-warning.php
+++ templates/account/membership-warning.php
@@ -1,7 +1,7 @@
-<?php do_action('opalmembership_membership_warning_content'); ?>
-<?php if(  !opalmembership_is_membership_valid() ) : ?>
-<div class="alert alert-danger">
-	<p><?php esc_html_e( 'Your membership package is expired please upgrade now','opalmembership' );?></p>
-	<p><a href="<?php echo opalmembership_get_membership_page_uri();?>"><?php esc_html_e( 'Click to this link to see plans','opalmembership' );?></a></p>
-</div>
-<?php  endif; ?>
+<?php do_action( 'opalmembership_membership_warning_content' ); ?>
+<?php if ( ! opalmembership_is_membership_valid() ) : ?>
+    <div class="alert alert-danger">
+        <p><?php esc_html_e( 'Your membership package is expired please upgrade now.', 'opalmembership' ); ?></p>
+        <p><a href="<?php echo opalmembership_get_membership_page_uri(); ?>"><?php esc_html_e( 'Click to this link to see plans', 'opalmembership' ); ?></a></p>
+    </div>
+<?php endif; ?>
